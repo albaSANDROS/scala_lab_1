716d17783d1361c5fcc8394d35669b8a54375210
albaSANDROS <61543351+albaSANDROS@users.noreply.github.com>
2020-05-10 15:02:06 +0300

    10.05.2020
    added 2nd lab

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,13 +2,31 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 
 object main {
-/* нахождение среднего арифметического введенных значений */
- // println("Enter range of array:")
- // val range: Int = StdIn.readInt()
- // println("Enter array:")
- // val numbers: List[Int] = List.fill(range){
- //   StdIn.readInt()
- //}
+/*
+*
+*  3.	Найти среднее арифметическое.
+*  3.	Инвертировать строку.
+*  3.	Найти сумму всех положительных чисел в коллекции.
+*  3.	Реализовать функцию, которая возвращает предпоследний элемент списка.
+*  3.	Преобразовать Map таким образом, чтобы ключи оригинального Map стали значениями преобразованного, и наоборот.
+*
+*/
+  println("Enter range of List:")
+  val range: Int = StdIn.readInt()
+  println("Enter list:")
+  val numbers: List[Int] = List.fill(range) {
+    StdIn.readInt()
+  }
+
+  println("Enter 1st element (INT) for MAP function:")
+  val element1: Int = StdIn.readInt();
+  println("Enter 2nd element (INT) for MAP function:")
+  val element2: Int = StdIn.readInt();
+  println("Enter 1st key (TEXT) for MAP function:")
+  val key1: String = StdIn.readLine();
+  println("Enter 2nd key (TEXT) for MAP function:")
+  val key2: String = StdIn.readLine();
+  val map = Map [Int, String](element1 -> key1, element2 -> key2)
  //
  //
  // def Average (list: List[Int]): Double = {
@@ -38,35 +56,56 @@ object main {
  // }
 
 
-  /* Реверс введенной строки */
-   println("Enter string:")
-   val text: String = StdIn.readLine()
+// /* Реверс введенной строки */
+//  println("Enter string:")
+//  val text: String = StdIn.readLine()
 
-  def reverse(s: String): String = {  // рекурсивная
-    if (s.isEmpty) ""
-    else reverse(s.tail) + s.head
-  }
+// def reverse(s: String): String = {  // рекурсивная
+//   if (s.isEmpty) ""
+//   else reverse(s.tail) + s.head
+// }
 
-  def tailReverse(str: String): String = {
-    @tailrec
-    def reverse(str: String, r: String): String =
-    {
-      str match{
-        case "" => r
-        case s => reverse(s.tail, s.head+r)
-      }
+// def tailReverse(str: String): String = {
+//   @tailrec
+//   def reverse(str: String, r: String): String =
+//   {
+//     str match{
+//       case "" => r
+//       case s => reverse(s.tail, s.head+r)
+//     }
+//   }
+//   reverse(str, "")
+// }
+
+// def reverse2(s: String) : String =
+//   (for(i <- s.length - 1 to 0 by -1) yield s(i)).mkString // рекурсия
+
+  /* Сумма позитивных элементов List */
+  def posSum(list: List[Int]): Int = {
+    val positive: PartialFunction[Int, Int] = {
+    case x: Int if x > 0 => x
     }
-    reverse(str, "")
+    list.collect(positive).sum
   }
 
-  def reverse2(s: String) : String =
-    (for(i <- s.length - 1 to 0 by -1) yield s(i)).mkString // рекурсия
+  /* Нахождение предпоследнего элемента List*/
+  def preLastFind(list: List[Int]): Int = {
+    list.foldLeft((list.head, list.tail.head))((r, c) => (r._2, c))._1
+  }
 
+  /* Реверс ключей и значений Map*/
+  def reverse[T, U](map: Map[T, U]): Map[U, T] = {
+    if (map.isEmpty) Map()
+    Map() ++ map.map(_.swap)
+  }
     def main(args: Array[String]): Unit = {
-      println("Result");
-      println(reverse(text))
-      println(reverse2(text))
-      println(tailReverse(text))
+      println("Results");
+      print("Sum of positive numbers: ");
+      println(posSum(numbers));
+      print("Pre-last element: ");
+      println(preLastFind(numbers));
+      print("Map function: ");
+      print(map); print(" -> "); print(reverse(map));
     }
 
 }
